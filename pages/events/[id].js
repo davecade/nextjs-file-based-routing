7231efe89ebf0fdab0a58f565d9663ce15f2db2b
Dave <dave.cadelina@outlook.com>
2023-07-11 14:08:04 +1000

    event details

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContext from "../../components/event-detail/event-content";
+import { getEventById } from "../../data/dummy-data";
 
 const EventsDetailsPage = () => {
-    return <h1>EventsDetailsPage</h1>;
+	const router = useRouter();
+
+	const eventId = router.query.id;
+
+	const event = getEventById(eventId);
+
+	if (!event) {
+		return <p>No event found!</p>;
+	}
+
+	return (
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContext>
+				<p>{event.description}</p>
+			</EventContext>
+		</>
+	);
 };
 
 export default EventsDetailsPage;
